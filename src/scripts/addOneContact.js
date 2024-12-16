3b5de6449be022b186e87f35a6629c012a70cab7
Olena Matsak <161434803+matsak-olena-p94@users.noreply.github.com>
2024-12-16 17:18:30 +0200

    Initial commit

---
+++ src/scripts/addOneContact.js
@@ -0,0 +1,11 @@
+import {readContacts} from "../utils/readContacts.js";
+import {createFakeContact} from "../utils/createFakeContact.js";
+import {writeContacts} from "../utils/writeContacts.js";
+
+export const addOneContact = async () => {
+    const contactList = await readContacts();
+    const contact = createFakeContact();
+    await writeContacts([...contactList, contact]);
+};
+
+addOneContact();
---
+++ src/scripts/getAllContacts.js
@@ -0,0 +1,4 @@
+import { readContacts } from "../utils/readContacts.js";
+export const getAllContacts = () => readContacts();
+ 
+console.log(await getAllContacts());
